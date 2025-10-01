26ae09d3bb581ad44a36e6e8a1160d8f121467fc
nicolegut <nmg40@duke.edu>
2025-10-01 14:08:45 -0400

    testing tuples etc 9/30

--- Planning_Script.py
+++ Planning_Script.py
@@ -5,6 +5,8 @@
 
 ##Importing Packages
 import pandas as pd
+import arcpy, os, time
+
 
 # time, os, arcpy packages? - used in Hazen
 
@@ -14,6 +16,7 @@ UCMR_All = "UCMR5_All"
 ZIPCodes = "UCMR5_ZIPCodes"
 UCMR_data = pd.read_csv(f"{path}/{UCMR_All}.txt", sep="\t", encoding="latin1")
 ZIP_data = pd.read_csv(f"{path}/{ZIPCodes}.txt", sep="\t", encoding="latin1")
+CWS_data = C:\Duke\Year 2\MP\PFAS_MP.gdb\CWS_Points
 
 
 ## Zip code data need to make sure they have leading zeros!!
@@ -51,25 +54,79 @@ Filtered_UCMR = UCMR_data[
 # Combine_UCMR = pd.merge(ZIP_data, UCMR_data, on="PWSID", how="XXXXX")
 
 # Merge with ZIP codes
+# look at the duplicate values for pwsid and zip codes
 Combine_UCMR = Filtered_UCMR.merge(ZIP_data, on="PWSID", how="left")
 
-# thoughts on the merge
+UCMR_narm = Combine_UCMR.dropna(subset=["AnalyticalResultValue"])
+# checked for duplicates, no duplicate values
 
 
 ##### Breaking the data into compound/ source type of interest
-## think this might be a some sort of function --> filter(contaminant, source)
+## think this could be turned into some sort of function --> filter(contaminant, source)
 ## will create 4 dataframes
+
+# subset for contaminant/ water type and check for duplicate combinations
+
+UCMR_PFOA_GW = UCMR_narm[
+    (UCMR_narm["Contaminant"] == "PFOA") & (UCMR_narm["FacilityWaterType"] == "GW")
+]
+
+UCMR_PFOA_SW = UCMR_narm[
+    (UCMR_narm["Contaminant"] == "PFOA") & (UCMR_narm["FacilityWaterType"] == "SW")
+]
+UCMR_PFOS_GW = UCMR_narm[
+    (UCMR_narm["Contaminant"] == "PFOS") & (UCMR_narm["FacilityWaterType"] == "GW")
+]
+UCMR_PFOS_SW = UCMR_narm[
+    (UCMR_narm["Contaminant"] == "PFOS") & (UCMR_narm["FacilityWaterType"] == "SW")
+]
+
+
+# making tuples for combinations of pwsid/zip for each subset
+# checking if there are duplicate tuples
+# tuple test
+PFOA_GW_tuples = [
+    tuple(row)
+    for row in UCMR_PFOA_GW[["PWSID", "AnalyticalResultValue", "ZIPCODE"]].values
+]
+
+"""
+add into tuples the concentration data, 
+to see if the duplicates have differing concentrations
+
+is there value in keeping the pwsid/ zip uniques? i deleted but could put back
+"""
+
+# splitting duplicates
+PFOA_GW_dict = {}
+
+
+# defining a function to assess duplicate PWSID and ZIPCodes for our subset datasets
+def count_duplicates(dict, pairs):
+    for i in pairs:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+
+
+count_duplicates(PFOA_GW_dict, PFOA_GW_tuples)
+"""
+okay i have the distinct combinations of zip codes/pwsid/concentration data
+need to figure out how many zip codes/ which ones have differing values
+- could also be dates? do the same zips get sampled multiple times 
+
+-okay it is dates/ sample event codes - for PWSIDs that sample more than once in the compliance
+- determine how many of these there are?
+
+"""
+
 
 ##### Subset the data into test/ control groups
 ## probably still in pandas
 ## CONSIDER: for subset - look into what is a good proportion to subset/ even spatial sampling
 ##will create 8 dataframes
-UCMR_PFOA_GW = UCMR_data[
-    (UCMR_data["Contaminant"] == "PFOA") & (UCMR_data["FacilityWaterType"] == "GW")
-]
-# UCMR_PFOA_SW =
-# UCMR_PFOS_GW =
-# UCMR_PFOS_SW =
+
 
 #### Output of Data Wrangling:
 """
