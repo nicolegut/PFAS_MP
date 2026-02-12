bf447c813557b5dcb796e055317d7463cbf4235a
nicolegut <nmg40@duke.edu>
2026-02-12 17:53:16 -0500

    new plots

--- SumStat_Plots.py
+++ SumStat_Plots.py
@@ -12,8 +12,13 @@ import seaborn as sns
 # moving back 2 folders to access the MP folder
 # current workspace: 'c:/Duke/Year 2/MP/Scripts/PFAS_MP'
 # new workspace: 'c:/Duke/Year 2/MP'
+"""
+Run these two lines below to set the current working directory!! 
+Then comment back out to avoid pushing the wd back too much
+"""
+
 # wd_path = Path.cwd().parents[1]
-# .chdir(wd_path)
+# os.chdir(wd_path)
 
 # reading in the RMSE/ MAE data
 # subsetting the data
@@ -21,12 +26,21 @@ import seaborn as sns
 # data = pd.read_csv("./Interpolation_testing/Int_Stats.csv")
 # all_sorted_data = data.apply(lambda col: col.sort_values().values)
 
-data = pd.read_csv("./Interpolation_testing/Top15_OrdKrig_MethodComp.csv")
+data = pd.read_csv("./Interpolation_testing/PFOA_SW/PFOA_SW_Top15_Comps.csv")
 
-top3_data = data.iloc[0:3, :]
+# data = pd.read_csv("./Interpolation_testing/PFOS_SW/PFOS_SW_Top15_Comps.csv")
+# data = pd.read_csv("./Interpolation_testing/PFOS_GW/PFOS_GW_Top15_Comps.csv")
+
+data_ukrm = data.iloc[:, :-2]
+
+top5_data = data_ukrm.iloc[0:5, :]
+top1_data = data_ukrm.iloc[0:1, :]
 
 
 # functions
+##
+
+
 def long_method_stat_comp(data):
     df_long = data.melt(var_name="method_stat", value_name="value").dropna()
     df_long["method"] = df_long["method_stat"].str.replace(
@@ -36,9 +50,9 @@ def long_method_stat_comp(data):
     return df_long
 
 
-def plot_interp_RMSE(df, top_df):
+def plot_interp_RMSE(df, df2, df3):
 
-    sns.set(style="whitegrid", font_scale=1.2)
+    sns.set(style="whitegrid", font_scale=1.3)
 
     plt.figure(figsize=(8, 6))
 
@@ -46,7 +60,7 @@ def plot_interp_RMSE(df, top_df):
         data=df[df["metric"] == "RMSE"],
         x="method",
         y="value",
-        width=0.5,
+        width=0.6,
         showfliers=False,
     )
 
@@ -61,13 +75,23 @@ def plot_interp_RMSE(df, top_df):
     )
     # top 3
     sns.stripplot(
-        data=top_df[top_df["metric"] == "RMSE"],
+        data=df2[df2["metric"] == "RMSE"],
+        x="method",
+        y="value",
+        color="orange",
+        alpha=1,
+        jitter=True,
+        size=7,
+    )
+
+    sns.stripplot(
+        data=df3[df3["metric"] == "RMSE"],
         x="method",
         y="value",
         color="red",
         alpha=1,
         jitter=True,
-        size=5,
+        size=9,
     )
 
     plt.ylabel("RMSE (ppt)")
@@ -77,9 +101,9 @@ def plot_interp_RMSE(df, top_df):
     return plt.show()
 
 
-def plot_interp_MAE(df, top_df):
+def plot_interp_MAE(df, df2, df3):
 
-    sns.set(style="whitegrid", font_scale=1.2)
+    sns.set(style="whitegrid", font_scale=1.3)
 
     plt.figure(figsize=(8, 6))
 
@@ -87,7 +111,7 @@ def plot_interp_MAE(df, top_df):
         data=df[df["metric"] == "MAE"],
         x="method",
         y="value",
-        width=0.5,
+        width=0.6,
         showfliers=False,
     )
 
@@ -102,13 +126,23 @@ def plot_interp_MAE(df, top_df):
     )
 
     sns.stripplot(
-        data=top_df[top_df["metric"] == "MAE"],
+        data=df2[df2["metric"] == "MAE"],
+        x="method",
+        y="value",
+        color="orange",
+        alpha=1,
+        jitter=True,
+        size=7,
+    )
+
+    sns.stripplot(
+        data=df3[df3["metric"] == "MAE"],
         x="method",
         y="value",
         color="red",
         alpha=1,
         jitter=True,
-        size=5,
+        size=9,
     )
 
     plt.ylabel("MAE (ppt)")
@@ -128,25 +162,12 @@ def remove_outliers_iqr(col):
 
 
 ##outlier removal
-
-data_no_outliers = data.apply(remove_outliers_iqr)
-
-
-long_out_rm = long_method_stat_comp(data_no_outliers)
-plot_interp_MAE(long_out_rm)
-plot_interp_RMSE(long_out_rm)
+# data_no_outliers = data.apply(remove_outliers_iqr)
 
 
-# top 10
-top15 = data_no_outliers.iloc[0:15]
-long_15 = long_method_stat_comp(top15)
-plot_interp_MAE(long_15)
-plot_interp_RMSE(long_15)
-
-print(data_no_outliers.notna().sum())
-
-
-long_data_method_comp = long_method_stat_comp(data)
-long_topdata_mcomp = long_method_stat_comp(top3_data)
-plot_interp_MAE(long_data_method_comp, long_topdata_mcomp)
-plot_interp_RMSE(long_data_method_comp, long_topdata_mcomp)
+# for the plots with two levels of coloring
+long_15 = long_method_stat_comp(data_ukrm)
+long_5 = long_method_stat_comp(top5_data)
+long_1 = long_method_stat_comp(top1_data)
+plot_interp_MAE(long_15, long_5, long_1)
+plot_interp_RMSE(long_15, long_5, long_1)
